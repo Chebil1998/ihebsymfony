d8232425da8dad35024da20008c39d7445ebae57
epi edu <epi.edu@Epi.Etudiant>
2021-11-11 14:59:33 +0100

    le deuxieme

--- src/Controller/JobController.php
+++ src/Controller/JobController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Job;
 
 class JobController extends AbstractController
 {
@@ -47,6 +48,20 @@ class JobController extends AbstractController
      */
     public function ajouter(): Response
     {
+$job=new Job();
+$job->setTitle('developpeur symfony');
+$job->setCompany('sloth-lab');
+$job->setDescription('nous recherchons un developpeur symfony expert disponible sur sousse');
+$job->setIsactivated(1);
+$job->setExpiresat(new \DateTime());
+// $job->setEmail('iheb.chebil@hotmail.fr');
+
+
+$em=$this->getDoctrine()->getManager();
+$em->persist($job);
+$em->flush();
+
+
         return $this->render('job/ajouter.html.twig', [
             'controller_name' => 'JobController',
 
@@ -75,7 +90,7 @@ $listjobs=array(
 
 
 );
-return $this->render('job/sidebar.html.twig',['mymenu'=>$listjobs]);
+return $this->render('job/sidebar.html.twig',['listjobs'=>$listjobs]);
 }
 
 }
